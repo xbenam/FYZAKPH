74bf2a10b567ef28d437f2c750284af3d8efc9e7
xbenam <xbenam@stuba.sk>
2022-03-28 10:23:47 +0200

    hw05

--- Cvika/Cviko5/Cviko5.c
+++ Cvika/Cviko5/Cviko5.c
@@ -23,7 +23,7 @@
 *                      x => t1 = (2 * V0 * sin(alfa)) / g
 * x(t1) = d = (2 * V0^2 * cos(alfa) * sin(alfa)) / g
 */
-const int icaskrok = 1000/60;
+const int icaskrok = 1000/30;
 float Lmax = 2000.0;
 
 float x = 0.0;
@@ -35,6 +35,7 @@ float ty = 0.0;
 float alfa = 0.0;
 float velocity = 0.0;
 float speed = 0.0;
+float platform = 0.0;
 
 float gravity = -9.81;
 
@@ -46,11 +47,11 @@ float maxX = -9999.0;
 
 void aktualizuj (const int ihod )
 {
-    if(ihod==1)Sleep(500);
+    if(ihod==1)Sleep(1500);
     glutPostRedisplay ();
     
-    if(y < -(maxY / 2) + 5) {y = -(maxY / 2) + 5;return;}
-    if(y > -(maxY / 2) + 5){
+    if(y < -(maxY / 2) + 2.5) {y = -(maxY / 2) + 2.5;return;}
+    if(y > -(maxY / 2) + 2.5){
         x += (cos(alfa * 3.14 / 180) * velocity);
         y += (sin(alfa * 3.14 / 180) * velocity + 0.5 * gravity * ihod);
     }
@@ -94,6 +95,25 @@ void strela ()
     glPointSize(5);
     glMatrixMode ( GL_MODELVIEW );
 
+    glLoadIdentity ();
+
+    glColor3f (1.0 , 1.0 , 1.0);
+
+    glBegin ( GL_LINES );
+    glVertex2f (-(maxX / 2), ty - 2.5);
+    glVertex2f ((maxX / 2), ty - 2.5);
+    glEnd ();
+
+    glLoadIdentity ();
+
+    glColor3f (1.0 , 1.0 , 1.0);
+
+    glBegin ( GL_LINES );
+    glVertex2f (-(maxX / 2), ty - 2.5);
+    glVertex2f (-(maxX / 2), ty + platform);
+    glEnd ();
+
+
     glLoadIdentity ();    
     
     glColor3f (1.0 , 0.0 , 0.5);
@@ -119,11 +139,14 @@ int main (int argc , char ** argv )
     alfa = atof(argv[1]);
     velocity = atof(argv[2]);
     speed = atof(argv[3]);
+    platform = atof(argv[4]);
 
     int i = 1;
 
     maxY = -9999.0;
     maxX = -9999.0;
+
+    y += platform;
 
     while (y > 0){
         if(y < 0.0) y = 0;
@@ -142,7 +165,7 @@ int main (int argc , char ** argv )
     {
         tankX = distance;
         x = 0;
-        y = 0.1;
+        y = 0.1 + platform;
         i = 1;
         while(y>0){
             x += cos(alfa * 3.14 / 180) * velocity;
@@ -156,10 +179,10 @@ int main (int argc , char ** argv )
     printf("d1: %f\nd2: %f", distance, maxX);
     
     maxX = distance;
-    x = -(maxX / 2) + 5;
-    y = -(maxY / 2) + 5.1;
-    tx = (maxX / 2) - 5;
-    ty = -(maxY / 2) + 5.1;
+    x = -(maxX / 2);
+    y = -(maxY / 2) + 0.1 + platform;
+    tx = (maxX / 2);
+    ty = -(maxY / 2) + 0.1;
 
     glutInit (& argc , argv );
     glutInitDisplayMode ( GLUT_DOUBLE );
